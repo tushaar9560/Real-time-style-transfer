67281b645530c8d23ab38ca28e3ae84115d6e5f1
Tushaar Sharma <tushaar.sharma05@gmail.com>
2024-02-28 14:57:02 +0530

    added pred parser

--- src/pipelines/predict.py
+++ src/pipelines/predict.py
@@ -3,3 +3,8 @@ import torch
 import torchvision.transforms as transforms
 import numpy as np
 from PIL import Image
+from src.components.network import image_encoder, decoder
+from src.components.stylized.single import MulLayer
+from src.logger import logging
+from src.exception import CustomException
+
